b599ef52680c37032457366fdfe79ae2b5c3eda5
Mike Ajala <agmt5989@gmail.com>
2019-10-20 04:30:10 +0100

    first-ish

--- intropyproject-classify-pet-images/check_images.py
+++ intropyproject-classify-pet-images/check_images.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
-#
-# TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+#                                                                            
+# PROGRAMMER: Mike Ajala
+# DATE CREATED: 10/20/2019
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
